84e834d8788f959fd715e6974113638e2c9f056d
zhangmeng <zhangmeng5337@126.com>
2021-12-23 16:46:56 +0800

    add eeg

---
+++ hdyl/code/EEG/inc/app_ble.h
@@ -0,0 +1,6 @@
+#ifdef BLE_H
+#define BLE_H
+void app(void);
+
+#endif
+
---
+++ hdyl/code/EEG/inc/ble.h
@@ -0,0 +1,18 @@
+#ifndef BLE_H
+#define BLE_H
+#include "main.h"
+
+#define Success 0
+#define Failure 1
+
+#define CONFIG_MODE 0
+#define TRANSMIMT_MODE 1
+#define CONNECT_STA   2 
+#define DIS_CONNECT  3
+#define DATA_STA 4
+#define WAKUP 5
+#define SLEEP 6
+unsigned int sendCommand(char *Command, char *Response, uint32_t Timeout, unsigned char Retry);
+
+#endif
+
