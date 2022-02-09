c160ea73d7cd8b9c14fcf80c78e0cdde18ded44e
zhangmeng5337@126.com <zhangmeng5337@126.com>
2022-02-09 21:10:58 +0800

    ble

---
+++ hdyl/code/BLE/b85m_ble_sdk_trans/vendor/b85m_m1s1/app_ui.h
@@ -0,0 +1,138 @@
+/********************************************************************************************************
+ * @file	app_ui.h
+ *
+ * @brief	This is the header file for BLE SDK
+ *
+ * @author	BLE GROUP
+ * @date	2020.06
+ *
+ * @par     Copyright (c) 2020, Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ *          All rights reserved.
+ *
+ *          Redistribution and use in source and binary forms, with or without
+ *          modification, are permitted provided that the following conditions are met:
+ *
+ *              1. Redistributions of source code must retain the above copyright
+ *              notice, this list of conditions and the following disclaimer.
+ *
+ *              2. Unless for usage inside a TELINK integrated circuit, redistributions
+ *              in binary form must reproduce the above copyright notice, this list of
+ *              conditions and the following disclaimer in the documentation and/or other
+ *              materials provided with the distribution.
+ *
+ *              3. Neither the name of TELINK, nor the names of its contributors may be
+ *              used to endorse or promote products derived from this software without
+ *              specific prior written permission.
+ *
+ *              4. This software, with or without modification, must only be used with a
+ *              TELINK integrated circuit. All other usages are subject to written permission
+ *              from TELINK and different commercial license may apply.
+ *
+ *              5. Licensee shall be solely responsible for any claim to the extent arising out of or
+ *              relating to such deletion(s), modification(s) or alteration(s).
+ *
+ *          THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *          ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *          WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *          DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+ *          DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *          (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *          LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *          ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *          (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *          SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************************************/
+#ifndef APP_UI_H_
+#define APP_UI_H_
+
+
+extern  int key_not_released;
+extern 	int	master_pairing_enable;
+extern 	int master_unpair_enable;
+
+extern	int master_disconnect_connhandle;
+
+extern	int master_auto_connect;
+extern	int user_manual_pairing;
+#define BUTTON_LONG_TIME   300
+#define BUTTON_FILTER_TIME  5
+#define KEY_START_HOLD   1
+#define KEY_START_DOWN   2
+#define KEY_PAIR         3
+#define KEY_UNPAIR       4
+
+#define AUTO_PAIR    1
+
+#define TIMEOUT_PERIOD 40
+#define M_ON_PERIOD 600
+#define M_OFF_PERIOD 100
+#define PKT_HEAD  0xfe
+#define MEASURE_PERIOD 1000
+typedef struct
+{
+	u8 start;
+	u8 Mstart;
+	u8 stop;
+	u16 dis;
+	u32 time;
+	u16 sum;
+	u32 tick;
+	u32 motor_tick;
+	u8 key;
+	u8 key_update;
+	u8 key_down_flag;
+}measure_stru;
+typedef struct
+{
+	u8 connection;
+	u8 disconnection;
+}ble_stru;
+
+/**
+ * @brief      keyboard task handler
+ * @param[in]  e    - event type
+ * @param[in]  p    - Pointer point to event parameter.
+ * @param[in]  n    - the length of event parameter.
+ * @return     none.
+ */
+void proc_keyboard (u8 e, u8 *p, int n);
+
+/**
+ * @brief      callback function of LinkLayer Event "BLT_EV_FLAG_SUSPEND_ENTER"
+ * @param[in]  e - LinkLayer Event type
+ * @param[in]  p - data pointer of event
+ * @param[in]  n - data length of event
+ * @return     none
+ */
+void  app_set_kb_wakeup (u8 e, u8 *p, int n);
+
+
+
+/**
+ * @brief      keyboard initialization
+ * @param[in]  none
+ * @return     none.
+ */
+void keyboard_init(void);
+
+
+
+/**
+ * @brief   BLE Unpair handle for master
+ * @param   none.
+ * @return  none.
+ */
+void proc_master_role_unpair(void);
+void user_gpio_init(void);
+void measure_start(void);
+void mesure_proc(void);
+void ble_status(u8 flag);
+ble_stru *GetBle_status(void);
+void parase(u8 tmp);
+u8 getsn(void);
+
+
+
+
+#endif /* APP_UI_H_ */
